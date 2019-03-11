657154544f3dfff4f97e989d7961e4256cd79543
vicdubbeld <vicdubbeld@gmail.com>
2019-03-11 17:42:47 -0400

    third push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,39 +5,32 @@
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guessesLeft = 10;
 var guessesSoFar = 0;
 
-// alphabet variable
+// alphabet array (there has to be an easier way)
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-var alphabet:Array = ("abcdefghijklmnopqrstuvwxyz").split("");
-trace("this is the letter b...", alphabet[1]);
-trace(alphabet);
-
-// function genCharArray(charA, charZ) {
-//     var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
-//     for (; i <= j; ++i) {
-//         a.push(String.fromCharCode(i));
-//     }
-//     return a;
-// }
-// genCharArray('a', 'z');
 
 // random letter generator
 
-    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
     console.log(computerChoice);
 
 // pressing a key
+
 document.onkeypress = function(event) {
     var userGuess = event.key;
 
     //if it matches the computer's choice, wins will increase wins by 1
+
     if (userGuess === computerChoice) {
         wins++;
     }
     //if it does not match computer's choice, guessesLeft will decrease by 1
+
     else {
         guessesLeft--;
     }
